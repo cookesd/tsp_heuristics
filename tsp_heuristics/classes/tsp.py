d74306e0055466fc458dd405b481677855830bb6
cookesd <stuart.d.cooke@gmail.com>
2020-12-11 12:21:17 -0600

    initial tsp class

---
+++ tsp_heuristics/classes/tsp.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 11 12:12:13 2020
+
+@author: dakar
+"""
+
+import numpy as np
+import pandas as pd
+import math
+import itertools
+
+class TSP:
+    def __init__(dist_mat):
+        pass
